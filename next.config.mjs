953ffd1c913e6ab07aae4e72c0c217c65dc94d9e
Akash Moradiya <akashmoradiya3444@gmail.com>
2024-12-01 17:52:50 +0530

    update config

--- next.config.mjs
+++ next.config.mjs
@@ -14,7 +14,9 @@ const nextConfig = {
     ],
   },
   experimental: {
-    outputFileTracingRoot: path.join(__dirname, "../../"),
+    outputFileTracingIncludes: {
+      "api/**/*.ts": ["src/**/*"],
+    },
   },
 };
 
