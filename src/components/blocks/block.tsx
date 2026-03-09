f473fb89c009c2f1ac3633eb117dcabb915e9a76
akash-moradiya <akash.moradiya@smallest.ai>
2026-03-09 21:03:13 +0530

    remove pro blocks

--- src/components/blocks/block.tsx
+++ src/components/blocks/block.tsx
@@ -1,18 +1,13 @@
 "use client";
 
-import { Gem } from "lucide-react";
-import Link from "next/link";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
-import { BLOCK_PRICING, type RegistryBlock } from "@/config/registry";
+import type { RegistryBlock } from "@/config/registry";
 import { BlockProvider } from "@/providers/block-provider";
-import { Button } from "../ui/button";
 import { BlockCodeExplorer } from "./block-code-explorer";
 import BlockPreview from "./block-preview";
 import BlockToolbar from "./block-toolbar";
 
 export function Block({ block }: { block: RegistryBlock }) {
-  const isFree = !block.pricing || block.pricing === BLOCK_PRICING.free;
-
   return (
     <BlockProvider key={block.name} name={block.name}>
       <div className="mx-auto w-full max-w-(--breakpoint-2xl) py-8">
@@ -21,33 +16,19 @@ export function Block({ block }: { block: RegistryBlock }) {
             <div className="font-medium text-lg">{block.title}</div>
             <div className="flex items-end gap-3">
               <BlockToolbar />
-              {isFree && (
-                <TabsList className="h-8 max-md:ms-auto max-md:hidden">
-                  <TabsTrigger value="preview">Preview</TabsTrigger>
-                  <TabsTrigger value="code">Code</TabsTrigger>
-                </TabsList>
-              )}
-              {!isFree && (
-                <Button asChild size="sm">
-                  <Link
-                    href={"https://pro.shadcnui-blocks.com/#pricing"}
-                    target="_blank"
-                  >
-                    <Gem /> Get Access
-                  </Link>
-                </Button>
-              )}
+              <TabsList className="h-8 max-md:ms-auto max-md:hidden">
+                <TabsTrigger value="preview">Preview</TabsTrigger>
+                <TabsTrigger value="code">Code</TabsTrigger>
+              </TabsList>
             </div>
           </div>
 
           <TabsContent value="preview">
             <BlockPreview />
           </TabsContent>
-          {isFree && (
-            <TabsContent className="max-md:hidden" value="code">
-              <BlockCodeExplorer />
-            </TabsContent>
-          )}
+          <TabsContent className="max-md:hidden" value="code">
+            <BlockCodeExplorer />
+          </TabsContent>
         </Tabs>
       </div>
     </BlockProvider>
