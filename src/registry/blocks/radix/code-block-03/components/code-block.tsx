19588ae76537fcd6c2e85905024f2c289170005e
akash3444 <akashmoradiya3444@gmail.com>
2026-05-03 22:07:00 +0530

    fix: code block overscroll

--- src/registry/blocks/radix/code-block-03/components/code-block.tsx
+++ src/registry/blocks/radix/code-block-03/components/code-block.tsx
@@ -3,7 +3,6 @@
 import { Suspense, use } from "react";
 import { codeToHtml } from "shiki";
 import { cn } from "@/lib/utils";
-import { ScrollArea, ScrollBar } from "@/registry/ui/radix/scroll-area";
 import { Tabs, TabsList, TabsTrigger } from "@/registry/ui/radix/tabs";
 
 const code = `const user = {\n\tname: "John Doe",
@@ -39,22 +38,17 @@ function CodeBlockInner() {
     <div className="flex min-h-dvh items-center justify-center px-6">
       <div className="rounded-lg border border-border/70 bg-muted/70 p-1 pt-0">
         <Tabs defaultValue="profile-card.tsx">
-          <ScrollArea className="grid max-w-full">
-            <ScrollBar orientation="horizontal" />
-            <TabsList
-              className={cn(
-                "items-end gap-2 rounded-none bg-transparent ps-2 pt-0.5 pb-0",
-                "*:rounded-none *:border-0 *:border-b-2 *:text-muted-foreground",
-                "*:data-[state=active]:border-foreground *:data-[state=active]:bg-transparent *:data-[state=active]:text-foreground *:data-[state=active]:shadow-none!"
-              )}
-            >
-              <TabsTrigger value="profile-card.tsx">
-                profile-card.tsx
-              </TabsTrigger>
-              <TabsTrigger value="profile.tsx">profile.tsx</TabsTrigger>
-              <TabsTrigger value="page.tsx">page.tsx</TabsTrigger>
-            </TabsList>
-          </ScrollArea>
+          <TabsList
+            className={cn(
+              "items-end gap-2 rounded-none bg-transparent ps-2 pt-0.5 pb-0",
+              "*:rounded-none *:border-0 *:border-b-2 *:text-muted-foreground",
+              "*:data-[state=active]:border-foreground *:data-[state=active]:bg-transparent *:data-[state=active]:text-foreground *:data-[state=active]:shadow-none!"
+            )}
+          >
+            <TabsTrigger value="profile-card.tsx">profile-card.tsx</TabsTrigger>
+            <TabsTrigger value="profile.tsx">profile.tsx</TabsTrigger>
+            <TabsTrigger value="page.tsx">page.tsx</TabsTrigger>
+          </TabsList>
         </Tabs>
         <div
           className={cn(
